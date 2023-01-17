7076eac4e29b8ffa3d529c4e4ef573c8909774b8
BJR-Developer <freelancerbjr@gmail.com>
2023-01-17 10:56:21 +0600

    settings language and navbar network dropdown adjustment

--- components/partials/navbar/Settings.tsx
+++ components/partials/navbar/Settings.tsx
@@ -86,7 +86,7 @@ const Settings = ({ isShowing, setIsShowing }: Props) => {
                     <Listbox.Option
                       key={langIdx}
                       className={({ active }) =>
-                        `relative hover:bg-primary-blue-color rounded-lg  cursor-pointer select-none py-2 pl-10 pr-4 ${
+                        `relative  cursor-pointer select-none font-normal ${
                           active ? "text-gray-100" : "text-gray-100"
                         }`
                       }
@@ -95,20 +95,22 @@ const Settings = ({ isShowing, setIsShowing }: Props) => {
                       {({ selected }) => (
                         <>
                           <span
-                            className={`block truncate ${
-                              selected ? "font-medium" : "font-normal"
+                            className={`block hover:bg-primary-blue-color/[0.6] rounded-lg p-2 truncate ${
+                              selected
+                                ? "font-medium bg-primary-blue-color"
+                                : "font-normal"
                             }`}
                           >
                             {lang.name}
                           </span>
-                          {selected ? (
+                          {/* {selected ? (
                             <span className={styles.selectedLang}>
                               <CheckIcon
                                 className="h-5 w-5"
                                 aria-hidden="true"
                               />
                             </span>
-                          ) : null}
+                          ) : null} */}
                         </>
                       )}
                     </Listbox.Option>
