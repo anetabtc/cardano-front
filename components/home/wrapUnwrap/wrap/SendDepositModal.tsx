e4a6c74b4d17330b056d778ff89474dfe398bdaf
Suku <unaeternidad@gmail.com>
2023-04-19 18:56:01 -0300

    payment credential generation

--- components/home/wrapUnwrap/wrap/SendDepositModal.tsx
+++ components/home/wrapUnwrap/wrap/SendDepositModal.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import WrapUnwrapModal from "../WrapUnwrapModal";
+import styles from "../../../../styles/home/WrapUnwrap.module.css";
+import { paymentCredentialOf } from "lucid-cardano";
 
 interface Props {
   isOpen: boolean;
@@ -16,6 +18,16 @@ export default function SendDepositModal({
   onClick,
   onClose,
 }: Props) {
+  const convert = async () => {
+    
+    const cardanoAddress = (document.getElementById('cardanoaddr') as HTMLInputElement).value;
+    if (cardanoAddress) {
+      
+      const paymentCreds = paymentCredentialOf(cardanoAddress);   
+      (document.getElementById('paymentCredentials') as HTMLInputElement).value = paymentCreds.hash;
+    }
+  }
+
   return (
     <WrapUnwrapModal
       isOpen={isOpen}
@@ -35,15 +47,50 @@ export default function SendDepositModal({
       onClick={onClick}
     >
       <div className="flex gap-6 flex-col">
-        <p>
-          <span className="font-bold text-[#F7931A]">IMPORTANT:</span> Add your
-          Cardano address in the “Message (Optional)” section in your Moonshine
-          Wallet before sending this deposit.
+        <p className="text-[#03C04A] text-center">
+          <div className="font-bold text-2xl">Attention</div>
+          You must first convert your Cardano address to a <span className="font-bold">Payment Credential</span>&nbsp;
+          address and use this <span className="font-bold">Payment Credential</span> in the BTC transaction metadata.
         </p>
         <p>
-          This Cardano address will receive cBTC. If you do not add your Cardano
-          address into the message section of this transaction, you will not
-          receive cBTC.
+          <div
+            className={`w-5/6 inline-block pr-4`}
+          >
+            <input
+              className={styles.btcAddressInput}
+              id='cardanoaddr'>
+            </input>
+          </div>
+          <button
+            className={`w-1/6 bg-primary-blue-color hover:bg-primary-blue-color/80 transition-all gap-2 justify-center font-semibold font-nunito-sans text-gray-50 text-center p-2 rounded-lg text-base`}
+            onClick={convert}>
+            Convert
+          </button>
+          <div
+            className={`w-5/6 inline-block pr-4`}
+          >
+            <input
+              readOnly
+              className={styles.btcAddressInput}
+              id='paymentCredentials'>
+            </input>
+          </div>
+          <button
+            className={`w-1/6 bg-primary-blue-color hover:bg-primary-blue-color/80 transition-all gap-2 justify-center font-semibold font-nunito-sans text-gray-50 text-center p-2 rounded-lg text-base`}
+            onClick={() => { navigator.clipboard.writeText((document.getElementById('paymentCredentials') as HTMLInputElement)?.value) }}>
+            Copy
+          </button>
+        </p>
+        <p>
+          <span className="font-bold text-[#F7931A]">Attention:</span> Add your
+          Payment Credentials in the “Message (Optional)” section in your Moonshine
+          Wallet before sending this deposit. To get your Payment Credentials, please
+          add your Cardano wallet on the next field, and copy the field below
+        </p>
+        <p>
+          This Payment Credentials will make you receive cBTC. If you do not add your Payment
+          Credentials into the message section of this transaction, you will not
+          receive cBTC into your Cardano address.
         </p>
         <hr></hr>
         <p>In a single transaction, send {amount} BTC to</p>
